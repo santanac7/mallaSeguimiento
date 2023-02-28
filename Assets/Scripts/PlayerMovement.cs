b4d58e00a0232c6c97ab5f7570a10d7ec8f6569c
CarlosPC <santanavargasc@gmail.com>
2023-02-27 23:03:34 -0500

    Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,16 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float xSpeed;
     float xMove;
 
+    // Jump
+    [SerializeField] float jumpPower;
+    [SerializeField] float ratioDetect;
+    [SerializeField] Transform centerDetect;
+    [SerializeField] LayerMask maskDetect;
+    bool jumpActive;
+    bool grounded;
+
+
+
     void Start()
     {
         pRb = GetComponent<Rigidbody2D>();
@@ -15,7 +25,33 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        // xMove
         xMove = Input.GetAxisRaw("Horizontal");
         pRb.velocity = new Vector2(xMove * xSpeed, pRb.velocity.y);
+
+        // Jump
+        if ( Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            jumpActive = true;
+        }
+    }
+
+    private void FixedUpdate() {
+        grounded = Physics2D.OverlapCircle(centerDetect.position, ratioDetect, maskDetect);
+
+        if (jumpActive) {
+            pRb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            jumpActive = false;
+        }
+        
+    }
+
+
+
+
+    // Gizmo grounded detection
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(centerDetect.position, ratioDetect);
     }
 }
